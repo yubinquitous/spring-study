1e1d44d4998c10b08d1bff20a94c5d3d3ddd7a0f
yubinquitous <yb9854@naver.com>
2023-01-14 15:52:38 +0900

    MVC와 템플릿 엔진 (#3)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,12 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!");   // key:value 쌍으로 전달
         return "hello"; // hello.html로 넘어감
+    }
+
+    /* MVC와 템플릿 엔진 */
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
