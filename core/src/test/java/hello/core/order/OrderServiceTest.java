a4ce73b38b25ffd6eded3656a00de81648c079bd
yubinquitous <yb9854@naver.com>
2023-01-25 16:06:59 +0900

    주문과 할인 도메인 실행과 테스트 (#15)

---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L; // long type일 경우 null을 넣을 수 없어서 wrapper type인 Long type을 사용함
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
