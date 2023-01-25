5a3fd0bd4af0843e77b0334f556da96a90c49cdf
yubinquitous <yb9854@naver.com>
2023-01-25 20:34:56 +0900

    스프링으로 전환 (#17)

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -7,20 +7,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());  // 생성자 주입
     }
 
-    private MemoryMemberRepository memberRepository() {  // 중복 제거
+    @Bean
+    public MemoryMemberRepository memberRepository() {  // 중복 제거
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
