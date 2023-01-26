1d59e5f885473ccc5a7d857bb40c50761b91d54e
yubinquitous <yb9854@naver.com>
2023-01-26 22:49:29 +0900

    @Configuration과 싱글톤 (#21)

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -13,18 +13,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
+    // 예상
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+
+    // ConfigurationSingletonTest 실행 결과
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository => 한 번만 호출됨!
+    // call AppConfig.orderService
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());  // 생성자 주입
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {  // 중복 제거
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
