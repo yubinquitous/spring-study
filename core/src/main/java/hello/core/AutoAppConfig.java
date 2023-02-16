efd63f4fc30b582bff7c79ae8240a8f45776b3c0
yubinquitous <yb9854@naver.com>
2023-02-16 20:56:59 +0900

    [FIX] AutoAppConfig basePackages 삭제 (#23)

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
