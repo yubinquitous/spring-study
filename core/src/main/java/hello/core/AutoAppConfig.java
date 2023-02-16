202fd6cc5472d17ab606ffd6bbb7ef2fe8bad215
yubinquitous <yb9854@naver.com>
2023-02-16 19:49:29 +0900

    탐색 위치와 기본 스캔 대상 (#23)

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
 }
