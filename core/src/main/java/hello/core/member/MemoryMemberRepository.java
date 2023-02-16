6074af58e7f788d2547c7a74df8858182e0f7f38
yubinquitous <yb9854@naver.com>
2023-02-16 21:03:13 +0900

    중복 등록과 충돌 (#23)

--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Component  // memoryMemberRepository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
